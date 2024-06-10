be3836ed48b349631e37e4435430f6ed0d4db682
vijay <digitalxvijay@gmail.com>
2024-06-10 22:52:37 +0530

    Fix: Login UI

--- src/components/Login.js
+++ src/components/Login.js
@@ -4,12 +4,18 @@ const Login = () => {
   return (
     <div>
       <Header />
-      <div>
-      <img
-        src="https://assets.nflxext.com/ffe/siteui/vlv3/cacfadb7-c017-4318-85e4-7f46da1cae88/e43aa8b1-ea06-46a5-abe3-df13243e718d/IN-en-20240603-popsignuptwoweeks-perspective_alpha_website_small.jpg"
-        alt="logo"
-      />
+      <div className="absolute">
+        <img
+          src="https://assets.nflxext.com/ffe/siteui/vlv3/cacfadb7-c017-4318-85e4-7f46da1cae88/e43aa8b1-ea06-46a5-abe3-df13243e718d/IN-en-20240603-popsignuptwoweeks-perspective_alpha_website_small.jpg"
+          alt="logo"
+        />
       </div>
+      <form className="w-3/12 text-white absolute p-12 bg-black bg-opacity-80 my-36 mx-auto right-0 left-0">
+      <h1 className="font-bold text-3xl py-4">Sign In</h1>
+        <input type="text" placeholder="Email Address" className="p-2 my-4 w-full bg-gray-700"/>
+        <input type="password" placeholder="Password" className="p-2 my-4 w-full bg-gray-700"/>
+        <button className="p-2 my-4 bg-red-700 w-full rounded-lg">Sign In</button>
+      </form>
     </div>
   );
 };
