5fb81b3fcd9617f13f21b03ee2a49e49e632695b
vijay <digitalxvijay@gmail.com>
2024-06-10 22:23:18 +0530

    netflix-gpt

---
+++ src/components/Login.js
@@ -0,0 +1,17 @@
+import Header from "./Header";
+
+const Login = () => {
+  return (
+    <div>
+      <Header />
+      <div>
+      <img
+        src="https://assets.nflxext.com/ffe/siteui/vlv3/cacfadb7-c017-4318-85e4-7f46da1cae88/e43aa8b1-ea06-46a5-abe3-df13243e718d/IN-en-20240603-popsignuptwoweeks-perspective_alpha_website_small.jpg"
+        alt="logo"
+      />
+      </div>
+    </div>
+  );
+};
+
+export default Login;
