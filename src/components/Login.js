68c66a3bc327cadc7447191b2e5922799de6d484
vijay <digitalxvijay@gmail.com>
2024-06-11 21:51:17 +0530

    Fix: Login UI

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,6 +1,12 @@
+import { useState } from "react";
 import Header from "./Header";
 
 const Login = () => {
+  const [isSignInForm, setIsSignInForm] = useState(true);
+
+  const toggleSignInForm = () => {
+    setIsSignInForm(!isSignInForm);
+  };
   return (
     <div>
       <Header />
@@ -11,10 +17,34 @@ const Login = () => {
         />
       </div>
       <form className="w-3/12 text-white absolute p-12 bg-black bg-opacity-80 my-36 mx-auto right-0 left-0">
-      <h1 className="font-bold text-3xl py-4">Sign In</h1>
-        <input type="text" placeholder="Email Address" className="p-2 my-4 w-full bg-gray-700"/>
-        <input type="password" placeholder="Password" className="p-2 my-4 w-full bg-gray-700"/>
-        <button className="p-2 my-4 bg-red-700 w-full rounded-lg">Sign In</button>
+        <h1 className="font-bold text-3xl py-4">
+          {isSignInForm ? "SignIn" : "SignUp"}
+        </h1>
+        <input
+          type="text"
+          placeholder="Email Address"
+          className="p-2 my-4 w-full bg-gray-700"
+        />
+        {isSignInForm && (
+          <input
+            type="text"
+            placeholder="Full name"
+            className="p-2 my-4 w-full bg-gray-700"
+          />
+        )}
+        <input
+          type="password"
+          placeholder="Password"
+          className="p-2 my-4 w-full bg-gray-700"
+        />
+        <button className="p-2 my-4 bg-red-700 w-full rounded-lg">
+          {isSignInForm ? "SignIn" : "SignUp"}
+        </button>
+        <p className="text-center" onClick={toggleSignInForm}>
+          {isSignInForm
+            ? "New to Netflix? Sign Up now"
+            : "Already Registered? Sign In now"}
+        </p>
       </form>
     </div>
   );
